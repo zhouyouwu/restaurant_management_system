ebbf0647407d502af1c8c6b2d5bc28d0400d2c83
zhouyouwu <zhouchaoyang@stu.xhu.edu.cn>
2020-04-21 09:52:08 +0800

    add 123.html

--- springtext/src/com/zhouyouwu/web/MainController.java
+++ springtext/src/com/zhouyouwu/web/MainController.java
@@ -9,6 +9,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class MainController {
     @RequestMapping(value = "/", method = GET)
     public String start(){
+
         return "index";
     }
     
