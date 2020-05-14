8a94663e17f7dfc9d448f7c2cb03a2647d9095e5
zhouyouwu <zhouchaoyang@stu.xhu.edu.cn>
2020-05-14 21:22:01 +0800

    mdf 新增用户登录检查函数

--- FlowerWeb/src/org/zhouyouwu/controller/AIController.java
+++ FlowerWeb/src/org/zhouyouwu/controller/AIController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.zhouyouwu.service.FileService;
 import org.zhouyouwu.service.PlantService;
@@ -19,6 +20,7 @@ public class AIController {
      * @param multipartFile
      * @return
      */
+    @ResponseBody
     @RequestMapping("/upFile")
     public String upFile(@RequestParam("imageFile") MultipartFile multipartFile){
         System.out.println("ok");
--- FlowerWeb/src/org/zhouyouwu/controller/UserController.java
+++ FlowerWeb/src/org/zhouyouwu/controller/UserController.java
@@ -1,13 +1,18 @@
 package org.zhouyouwu.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zhouyouwu.beans.UserBean;
 import org.zhouyouwu.service.RegisterService;
 
+import javax.validation.Valid;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -32,6 +37,7 @@ public class UserController {
      *
      * @param user
      */
+    @Valid
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public void register(@NonNull UserBean user){
         System.out.println(user.getUserId());
@@ -39,5 +45,13 @@ public class UserController {
         System.out.println(user.getPassword());
 
         RegisterService.addUser(user);
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public void login(UserBean userBean){
+        //JSONObject jsonObject = JSON.parseObject(json);
+        String userId = userBean.getUserId();//jsonObject.getString("userId");
+        String password = userBean.getPassword();//jsonObject.getString("password");
+        System.out.println(userId);
     }
 }
